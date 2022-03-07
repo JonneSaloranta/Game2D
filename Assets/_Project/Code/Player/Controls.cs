88eb7022c0ca8c8aa726b2fcba0a715a7023fd66
Jonne <saloranta.jonne@gmail.com>
2022-03-08 00:15:38 +0200

    Added horizontal movement

--- Assets/_Project/Code/Player/Controls.cs
+++ Assets/_Project/Code/Player/Controls.cs
@@ -6,13 +6,16 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
     [SerializeField] float jumpForce;
-
-    [SerializeField] private LayerMask platformLayerMask;
+    [SerializeField] float movementSpeed;
 
     private Rigidbody2D rb;
     private BoxCollider2D boxCollider2D;
 
     private PlayerControls playerControls;
+
+    private float horizontalInput;
+    private bool isFacingRight;
+
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
         boxCollider2D = GetComponent<BoxCollider2D>();
@@ -21,13 +24,35 @@ public class Controls : MonoBehaviour {
         playerControls.Movement.Jump.performed += ctx => Jump();
     }
 
+    private void Update() {
+        horizontalInput = playerControls.Movement.MovementHorizontal.ReadValue<float>();
+    }
+
+    private void FixedUpdate() {
+
+        rb.velocity = new Vector2(movementSpeed * horizontalInput, rb.velocity.y);
+
+        if (!isFacingRight && horizontalInput > 0) {
+            ChangeFacingDirection();
+        } else if (isFacingRight && horizontalInput < 0) {
+            ChangeFacingDirection();
+        }
+    }
+
     public void Jump() {
-        if (!isGrounded()) return;
+        if (!IsGrounded()) return;
         rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 
-    private bool isGrounded() {
+    private void ChangeFacingDirection() {
+        isFacingRight = !isFacingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+
+    private bool IsGrounded() {
         return transform.Find("GroundCheck").GetComponent<GroundCheck>().isGrounded;
     }
 
